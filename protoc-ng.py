c270e9665e903bad0b254f879fd06e6e3baf8129
Oleg Smolsky <oleg@smolsky.net>
2018-04-01 15:36:36 -0700

    parser: added field type validation
     - currently messages and enums are resolved
       + in both implicit C-style scopes, and
       + explicit fully-qualified paths
         - including imports!

--- protoc-ng.py
+++ protoc-ng.py
@@ -183,10 +183,58 @@ class FileNode(Node):
         self.statements = []
         self.imports = {}
         self.options = []
-        self.messages = []
+        self.messages = {}
         self.enums = {}
         self.name = fname
         self.namespace = ""
+        self.parent = None
+
+    def as_string(self):
+        assert(self.namespace)
+        s = "AST for " + self.name + ", namespace=" + self.namespace + "\n\n"
+
+        # Enums
+        for name, enum in self.enums.items():
+            s += enum.as_string(self.namespace) + "\n"
+
+        # Messages
+        for name, msg in self.messages.items():
+            s += msg.as_string(self.namespace)
+
+        return s
+
+    # Looks for the given fully-qualified field type 'ftype' in a top->down fashion by
+    # resolving every segment of the type's string.
+    def resolve_type(self, fq_type):
+        assert(fq_type)
+        assert(self.namespace)
+
+        if fq_type[0:len(self.namespace)] == self.namespace:
+            fq_type = fq_type[len(self.namespace):]
+            parts = fq_type.split('.')
+            front = parts.pop(0) # the remaining "."
+            assert(not front)
+            front = parts.pop(0)
+            assert(front)
+
+            if front in self.enums:
+                if len(parts) == 0:
+                    return ast.enums[front]
+                return None
+
+            if front in self.messages:
+                msg = self.messages[front]
+                if len(parts) == 0:
+                    return msg
+                return msg.resolve_type(".".join(parts))
+        else:
+            for file_name, file_ast in self.imports.items():
+                # TODO: this step should check the type prefix instead of searching
+                #       blindly, but that's tricky due to variable number of segments...
+                t = file_ast.resolve_type(fq_type)
+                if t:
+                    return t
+        return None
 
 class PackageNode(Node):
     def __init__(self, name):
@@ -202,29 +250,89 @@ class OptionNode(Node):
         self.value = value
 
 class MessageNode(Node):
-    def __init__(self, fq_name):
+    def __init__(self, fq_name, parent):
+        self.parent = parent        # the parent Message or None
         self.fq_name = fq_name
         self.fields = {}
         self.enums = {}
-        self.messages = []
+        self.messages = {}
 
-    def as_string(self, prefix = ""):
-        s = prefix + "Message: " + self.fq_name + "\n"
+    def name(self):
+        assert(self.fq_name)
+        return self.fq_name.split('.')[-1]
+
+    def as_string(self, namespace):
+        assert(namespace)
+        assert(namespace[-1] != '.')
+        assert(namespace + "." + self.name() == self.fq_name)
+
+        s = indent_from_scope(namespace) + "Message: "
+        global args
+        if args.fqdn:
+            s += self.fq_name  + "\n"
+        else:
+            s += self.name() + "\n"
 
         # Fields
         for id, field in self.fields.items():
-            s += field.as_string(prefix + "    ") + "\n"
+            s += field.as_string(namespace + "." + self.name()) + "\n"
+
+        # Enums
+        for name, enum in self.enums.items():
+            s += enum.as_string(namespace + "." + self.name()) + "\n"
 
         # Sub-messages
-        for sub_msg in self.messages:
-            s += sub_msg.as_string(prefix + "    ")
+        for name, sub_msg in self.messages.items():
+            s += sub_msg.as_string(namespace + "." + self.name())
 
         return s
+
+    # Looks for the given fully-qualified field type 'ftype' in a top->down fashion by
+    # resolving every segment of the type's string.
+    def resolve_type(self, fq_type):
+        assert(fq_type)
+
+        parts = fq_type.split('.')
+        assert(len(parts) > 0)
+        front = parts.pop(0)
+        assert(front)
+
+        if front in ast.enums:
+            if len(parts) == 0:
+                return ast.enums[ftype]
+            return None
+
+        if front in ast.messages:
+            msg = ast.messages[ftype]
+            if len(parts) == 0:
+                return msg
+            return msg.resolve_type(".".join(parts))
+
+        return None
 
 class EnumNode(Node):
     def __init__(self, fq_name):
         self.fq_name = fq_name
         self.values = {}
+
+    def name(self):
+        assert(self.fq_name)
+        return self.fq_name.split('.')[-1]
+
+    def as_string(self, namespace):
+        assert(namespace)
+        assert(namespace[-1] != '.')
+        assert(namespace + "." + self.name() == self.fq_name)
+
+        s = indent_from_scope(namespace) + "Enum: "
+
+        global args
+        if args.fqdn:
+            s += self.fq_name
+        else:
+            s += self.name()
+
+        return s + "(" + ", ".join(self.values.values()) + ")"
 
 class FieldNode(Node):
     def __init__(self, name, id, ftype, is_builtin, specifier):
@@ -236,8 +344,11 @@ class FieldNode(Node):
         if specifier and specifier.value == "repeated":
             self.is_repeated = True
 
-    def as_string(self, prefix = ""):
-        return prefix + \
+    def as_string(self, namespace):
+        assert(namespace)
+        assert(namespace[-1] != '.')
+
+        return indent_from_scope(namespace) + \
             "[" + str(self.id) + "] " + self.name + " : " + self.ftype +\
                 (" (repeated)" if self.is_repeated else "")
 
@@ -250,7 +361,7 @@ def parse_file(path):
     # First fine the file. Let's start with the given path and then search
     # the 'includes'.
     if not os.path.isfile(path):
-        for inc in __args.include:
+        for inc in args.include:
             next_path = inc
             assert(next_path)
             if next_path[-1] != '/':
@@ -273,8 +384,40 @@ def parse_file(path):
 # Utils
 #
 def log(verbosity, msg):
-    if __args.verbosity >= verbosity:
+    if args.verbosity >= verbosity:
         print(msg)
+
+# Looks for the given field type 'ftype' in the ever-widening message scopes (from inside
+# out).
+#
+# This function finds anything that can be used as a field:
+#   - message
+#   - enum
+def find_type(ast, ftype):
+	if not ast:
+		return None
+
+	if ftype in ast.messages:
+		return ast.messages[ftype]
+	if ftype in ast.enums:
+		return ast.enums[ftype]
+
+	return find_type(ast.parent, ftype)
+
+def find_top_parent(ast):
+    assert(ast)
+    if not ast.parent:
+        return ast
+    return find_top_parent(ast.parent)
+
+def indent(level):
+    return "    " * level
+
+def indent_from_scope(fq_name):
+    if not fq_name:
+        return ""
+    level = fq_name.count('.')
+    return indent(level)
 
 # Grammar:
 #  <input>         ::= <statement> [ <statement> ] EOF
@@ -308,10 +451,9 @@ def statement(ctx, file_node):
         file_node.options.append(option(ctx))
         log(2, "Parsed an 'option' statement: " + file_node.options[-1].name)
     elif keyword.type == Token.Type.Keyword and keyword.value == "message":
-        file_node.messages.append(message(ctx, file_node.namespace))
-        log(2, "Parsed a 'message' : " + file_node.messages[-1].fq_name)
+        message(ctx, file_node, file_node.namespace + ".")
     elif keyword.type == Token.Type.Keyword and keyword.value == "enum":
-        enum(ctx, file_node)
+        enum(ctx, file_node, file_node.namespace + ".")
     else:
         raise ValueError("Unexpected keyword while parsing the 'statement' rule: '" +
                 keyword.value + "'")
@@ -345,14 +487,18 @@ def option(ctx):
 
 # Grammar:
 #  <message>     ::= SCOPE_OPEN decl_list SCOPE_CLOSE
-def message(ctx, scope):
+def message(ctx, parent, scope):
     fq_name = ctx.consume_identifier(message.__name__).value
     if scope:
-        fq_name = scope + "." + fq_name
+        fq_name = scope + fq_name
 
-    ast = MessageNode(fq_name)
+    ast = MessageNode(fq_name, parent)
     ctx.consume_scope_open(message.__name__)
-    decl_list(ctx, ast, fq_name)
+
+    parent.messages[ast.name()] = ast
+    log(2, indent_from_scope(fq_name) + "Parsed a 'message' : " + ast.fq_name)
+
+    decl_list(ctx, ast, fq_name + ".")
     ctx.consume_scope_close(message.__name__)
     return ast
 
@@ -363,7 +509,7 @@ def decl_list(ctx, parent, scope):
         # Process sub-messages.
         if ctx.scanner.next() == Token.Type.Keyword and ctx.scanner.get().value == "message":
             ctx.consume_keyword(decl_list.__name__)
-            parent.messages.append(message(ctx, scope))
+            message(ctx, parent, scope)
             continue
 
         # This must be a normal field declaration.
@@ -381,18 +527,36 @@ def decl(ctx, parent, scope):
         spec = ctx.consume()
 
     if ctx.scanner.next() == Token.Type.DataType:
-        ftype = ctx.consume()
+        ftype = ctx.consume().value
         fname = ctx.consume_identifier(decl.__name__)
         is_builtin = True
     elif ctx.scanner.next() == Token.Type.Identifier:
-        # 1. handle the type name, possible fully qualified.
-        ftype = ctx.consume_identifier(decl.__name__)
+        # 1a. take the type name, possible fully qualified.
+        ftype = ctx.consume_identifier(decl.__name__).value
         while ctx.scanner.next() == Token.Type.Dot:
             ctx.consume()
             trailer = ctx.consume_identifier(decl.__name__)
-            ftype.value += "." + trailer.value
+            ftype += "." + trailer.value
 
-        # 2. handle the filed name
+        # 1b. verify the type:
+        #   - unqualified reference is expanded into a fully-qualified name.
+        if ftype.count(".") == 0:
+            resolved_type = find_type(parent, ftype)
+            if not resolved_type:
+                sys.exit("Failed to resolve message type: \"" + ftype +
+                    "\" on line " + str(ctx.scanner.line) + "\n\n\n" +
+                    find_top_parent(parent).as_string())
+            assert(resolved_type.name() == ftype)
+            ftype = resolved_type.fq_name
+        else:
+            resolved_type = find_top_parent(parent).resolve_type(ftype)
+            if not resolved_type:
+                sys.exit("Failed to resolve an FQ message type: \"" + ftype +
+                    "\" on line " + str(ctx.scanner.line) + "\n\n\n" +
+                    find_top_parent(parent).as_string())
+            assert(resolved_type.fq_name == ftype)
+
+        # 2. take the field name
         fname = ctx.consume_identifier(decl.__name__)
         is_builtin = False
     elif ctx.scanner.next() == Token.Type.Keyword and ctx.scanner.get().value == "enum":
@@ -407,43 +571,49 @@ def decl(ctx, parent, scope):
     ctx.consume_semi(decl.__name__)
     parent.fields[int(fid.value)] = FieldNode(fname.value,
                                               int(fid.value),
-                                              ftype.value,
+                                              ftype,
                                               is_builtin,
                                               spec)
-    log(2, "Parsed a 'field' declaration: " + fname.value)
+    log(2, indent_from_scope(scope) + "Parsed a 'field' declaration: " + fname.value)
 
 # Grammar:
 #  <decl>     ::= ENUM identifier SCOPE_OPEN <evalue-list> SCOPE_CLOSE
-def enum(ctx, parent, scope = ""):
-    ename = ctx.consume_identifier(enum.__name__)
+def enum(ctx, parent, scope):
+    name = ctx.consume_identifier(enum.__name__).value
+    fq_name = name
     ctx.consume_scope_open(enum.__name__)
-    ast = EnumNode(scope + ename.value)
-    parent.enums[ename.value] = ast
-    evalue_list(ctx, ast)
+    if scope:
+        assert(scope[-1] == '.')
+        fq_name = scope + name
+    ast = EnumNode(fq_name)
+
+    parent.enums[name] = ast
+    log(2, indent_from_scope(fq_name) + "Parsed an 'enum' statement: " + fq_name)
+
+    evalue_list(ctx, ast, scope)
     ctx.consume_scope_close(enum.__name__)
-    log(2, "Parsed an 'enum' statement: " + parent.enums[ename.value].fq_name)
 
 # Grammar:
 #  <evalue_list>     ::= <evalue> [ <evalue> ]
-def evalue_list(ctx, parent):
+def evalue_list(ctx, parent, scope):
     while ctx.scanner.next() != Token.Type.ScopeClose:
-        evalue(ctx, parent)
+        evalue(ctx, parent, scope + ".")
 
 # Grammar:
 #  <evalue>     ::= identifier EQALS number SEMI
-def evalue(ctx, enum):
+def evalue(ctx, enum, scope):
     fname = ctx.consume_identifier(evalue.__name__)
     ctx.consume_equals(evalue.__name__)
     eid = ctx.consume_number(evalue.__name__)
     ctx.consume_semi(evalue.__name__)
     enum.values[int(eid.value)] = fname.value
+    log(2, indent_from_scope(scope) + "Parsed an enum constant: " + fname.value)
 
 #
 # Semantic part.
 #
 def verify(file):
-    for msg in file.messages:
-        log(1, msg.as_string())
+    log(1, file.as_string())
     log(1, "Good!")
 
 #
@@ -456,9 +626,11 @@ parser.add_argument('-I', '--include', help='Include (search) directory',
                     action='append')
 parser.add_argument("-v", "--verbosity", help="increase output verbosity",
                     action="count", default=0)
+parser.add_argument("--fqdn", help="print fully-qualified message and enum types in AST",
+                    action='store_true')
 parser.add_argument('filename', metavar='filename',
                     help='Input file name')
-__args = parser.parse_args()
+args = parser.parse_args()
 
 ast = parse_file(sys.argv[1])
 verify(ast)
